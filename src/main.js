8c29bdc5ad0288481ab93534b29ce64269a1d0b5
Per Liedman <per.liedman@dotnetmentor.se>
2018-03-13 11:42:02 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+export {default as Control} from './components/Control.html'
+export {default as Geocoder} from './components/Geocoder.html'
+export {default as Itinerary} from './components/Itinerary.html'
+export {default as OSRMv1} from './osrm-v1'
+export {formatDistance, formatDuration, getLastWaypointIndexBefore} from './utils'
+export {default as Waypoint} from './waypoint'
